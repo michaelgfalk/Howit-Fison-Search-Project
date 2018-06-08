8cf76ce34eadc9c99d7b82ccad2d99b58abad8d6
Falkie <35712723+michaelgfalk@users.noreply.github.com>
2018-06-08 17:02:09 +1000

    Delete

--- learn-string-distance/extractTrainingSet.R
+++
@@ -1,8 +0,0 @@
-#Let's try and get a training set out of these files again...
-files.v <- dir("~/github/Howit-Fison-Search-Project/howFisPapers", full.names = T)
-library(XML)
-pairs.l <- lapply(files.v, function(x){
-  doc <- xmlTreeParse(x, useInternalNodes = T) #import xml file
-  #Now we need to get all the anchor elements.
-  a.ns <- getNodeSet(doc, "//d:div[@class='page-content']//d:a", namespaces = c(d = "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"))
-})
